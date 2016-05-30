9f2d670698e67ed4e6b420cf005c0a88e9f09bfb
RemiLelaidier <r.lelaidier@hotmail.fr>
2016-05-30 15:55:45 +0200

    Modif date heure

--- ServiceFichier.java
+++ ServiceFichier.java
@@ -1,15 +1,22 @@
-package chat;
+package TchatJAVA;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class ServiceFichier {
 	
 	// Attributs
+	private Date madate;
+	private Date monheure;
 	private FileWriter fw;
 	private BufferedWriter output;
 	private String adrFichier;
+	private String txtdate;
+	private String txtheure;
 
 	// Constructeur
 	public ServiceFichier(){
@@ -18,10 +25,14 @@ public class ServiceFichier {
 	
 	// Methode
 	public String enregistrer(String texte, String note) throws IOException{
-		adrFichier = System.getProperty("user.dir") + "/Sauvegarde.txt";
+		madate = new Date();
+		monheure = new Date();
+		txtdate = new SimpleDateFormat("yyyy_MM_dd", Locale.FRANCE).format(madate);
+		txtheure = new SimpleDateFormat("hh_mm", Locale.FRANCE).format(madate);
+		adrFichier = System.getProperty("user.dir") + "/Sauvegarde_du_"+ txtdate.toString() + "_" + txtheure.toString() +".txt";
 		fw = new FileWriter(adrFichier, true);
 		output = new BufferedWriter(fw);
-		String texteNote = texte +"\nNote : " + note + "/20";
+		String texteNote = texte +"\nNote : " + note + "/20" ;
 		output.write(texteNote);
 		output.flush();
 		output.close();
